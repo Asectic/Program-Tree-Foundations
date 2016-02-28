c5f7c20d468e246830b3959cffc76008c151a862
Allen Lee <allenyuklun.lee@mail.utoronto.ca>
2016-02-28 17:07:44 -0500

    Finished connecting the route to allow the client to send the code to the server and output the code. However, the compiling part is not complete yet

--- Webpage Files/compilerapp/routes/index.js
+++ Webpage Files/compilerapp/routes/index.js
@@ -6,10 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//display the textbox page where the code will be typed
 router.get('/compile', function(req, res, next){
 
 	res.render('compilecode');
 });
 
+//handle request to compile the code
+router.post('/compile', function(req, res, next){
+
+	var code = req.body.code;
+	res.send(code);
+
+});
 
 module.exports = router;
