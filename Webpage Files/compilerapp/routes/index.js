6a7a07f011d074a22df72c682980bce1cb8bd87d
Wenfeng <wenfeng99099@gmail.com>
2016-02-28 17:20:23 -0500

    saving string to file

--- Webpage Files/compilerapp/routes/index.js
+++ Webpage Files/compilerapp/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var string = "Hello World";
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,7 +18,14 @@ router.get('/compile', function(req, res, next){
 
 //handle request to compile the code
 router.post('/compile', function(req, res, next){
-
+	
+	fs.writeFile("code.c", string, function(err){
+		if(err){
+			return console.log(err);
+		}
+		console.log("The file was saved");
+	});
+	
 	var code = req.body.code;
 	res.send(code);
 
