4ed9bdbaa4ca38b24341706360a8111c06c0a65e
Allen Lee <allenyuklun.lee@mail.utoronto.ca>
2016-02-28 22:28:05 -0500

    Added the compiler into the index.js route directly to make the code compile

--- Webpage Files/compilerapp/compilerjs/exec.js
+++ Webpage Files/compilerapp/compilerjs/exec.js
@@ -4,15 +4,20 @@ module.exports = function compiler(filename){
 
 	//parse filename and add .exe to the end
 	var name = filename.substring(0, filename.length - 2) + '.exe';
+	var result ='';
 
 	//compiles the code with gcc and then executes the code
 	var child = exec('gcc ' + filename + ' -o' + name + '&& ' + name, function(err, stdout, stderr){
 	if (stderr){
 		console.log(stderr)
-		return(stderr);
+		result += stderr.toString();
+		console.log(result);
+		return result;
 	}else{
 		console.log(stdout)
-		return(stdout);
+		result += stdout.toString();
+		console.log(result);
+		return result;
 	}
 	});
 
--- Webpage Files/compilerapp/routes/index.js
+++ Webpage Files/compilerapp/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var compiler = require('../compilerjs/exec.js');
+//var compiler = require('../compilerjs/exec.js');
+var exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,7 +21,7 @@ router.post('/compile', function(req, res, next){
 	//save the users code that wants to be compiled
 	var code = req.body.code;
 	//save the user's name
-	var fileName = req.body.name + ".c"; 
+	var fileName = req.body.name.trim() + ".c"; 
 	
 	//write the code to the file
 	fs.writeFile(fileName, code, function(err){
@@ -30,11 +31,19 @@ router.post('/compile', function(req, res, next){
 		console.log("The file was saved");
 	});
 
-	//calls compiler to compile the code sent by client
-	var result = compiler(fileName);
+	//compiles the code and send result to client
+	var name = fileName.substring(0, fileName.length - 2) + '.exe';
 	
-	res.send(code);
-
+	//compiles the code with gcc and then executes the code
+	var child = exec('gcc ' + fileName + ' -o' + name + '&& ' + name, function(err, stdout, stderr){
+	if (stderr){
+		console.log(stderr.toString());
+		res.send(stderr.toString());
+	}else{
+		console.log(stdout.toString());
+		res.send(stdout.toString());
+	}
+	});
 });
 
 module.exports = router;
