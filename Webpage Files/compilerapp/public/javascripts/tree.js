76efcdf7a269656accc08c441b633ec0a925a57c
Flora Lee <flora.hjlee@gmail.com>
2016-02-29 16:44:26 -0500

    Update tree.js
    To current version

--- Webpage Files/compilerapp/public/javascripts/tree.js
+++ Webpage Files/compilerapp/public/javascripts/tree.js
@@ -1,26 +1,42 @@
-// Declaring variables
-var tree; 
+	// Declaring variables
+	var tree; 
+	
+	// Initializing values
+	tree = document.getElementById("tree"); 
+	node1 = createNode("Hello", "/compilerapp/views/contentCh1.ejs"); 
+	node2 = createNode("There", "http://www.facebook.com");
+	level1 = createLevel();
+	level2 = createLevel();
+	addToLevel(2, node1);
+	addToLevel(2, node2);
 
-// Initializing values
-tree = document.getElementById("tree"); 
-createNode("Hello", "/compile"); 
-createNode("There", "/compile");
-createNode("Good", "/compile"); 
-createNode("Bye", "/compile");
-// Function that creates a new node 
-function createNode(text, redirect){
-	// Create new elements needed for node
-	var newNode = document.createElement("div");
-	var text = document.createTextNode(text); 
-	
-	// Set id of new node		
-	newNode.setAttribute("class", "treeNode"); 
-	$(newNode).click(function () {
-			window.location = redirect;
-	});
-	
-	// Add them to the DOM
-	newNode.appendChild(text); 
-	tree.appendChild(newNode);  // This should probably not be here. 
-								// Instead we should return the node. 
-}
+
+	function createLevel(){
+		var newLevel = document.createElement("div");
+		newLevel.id = "Level" + tree.childNodes.length; 
+		tree.appendChild(newLevel);
+		return newLevel;
+	}
+
+	// Function that creates a new node 
+	function createNode(text, redirect){
+		// Create new elements needed for node
+		var newNode = document.createElement("div");
+		var text = document.createTextNode(text); 
+		
+		// Set id of new node		
+		newNode.setAttribute("class", "treeNode"); 
+		$(newNode).click(function () {
+    			window.location = redirect;
+		});
+		
+		// Add them to the DOM - which creates the node with text (blue)
+		newNode.appendChild(text); 
+		//tree.appendChild(newNode);
+		return newNode; 
+	}
+
+	function addToLevel(level, node){
+		var toAdd = document.getElementById("Level" + level);
+		toAdd.appendChild(node); 
+	}
