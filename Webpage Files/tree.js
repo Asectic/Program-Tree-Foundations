e32952dff811d39e40dc44d48e3088aed799bf05
c4khatta <shamama.khattak@mail.utoronto.ca>
2016-03-01 03:04:10 -0500

    Update tree.js
    Made the design of the tree more flexibe

--- Webpage Files/tree.js
+++ Webpage Files/tree.js
@@ -7,23 +7,25 @@
 	node2 = createNode("There", "http://www.facebook.com");
 	node3 = createNode("Top", "http://www.hotmail.com");
 	node4 = createNode("hi", "http://www.gmail.com");
-	node5 = createNode("hi", "http://www.google.com"); 
-	node6 = createNode("hi", "http://www.google.com"); 
-	node7 = createNode("hi", "http://www.google.com"); 
+	//node5 = createNode("hi", "http://www.google.com"); 
+	//node6 = createNode("hi", "http://www.google.com"); 
+	//node7 = createNode("hi", "http://www.google.com"); 
 	level1 = createLevel();
 	level2 = createLevel();
-	level3 = createLevel(); 
-	//createSubsection(2, 1); 
+	//level3 = createLevel(); 
+	createSubsection(1, 1); 
+	createSubsection(2, 2); 
 
-	addToLevel(1, node3);
-	addToLevel(2, node1);
-	addToLevel(2, node2);
-	addToLevel(3, node5); 
-	addToLevel(3, node4); 
-	addToLevel(3, node1); 
-	addToLevel(3, node6);
-	addToLevel(2, node4); 
-	addToLevel(3, node7); 
+	addToLevel(1, 1, node1);
+	addToLevel(2, 1, node2);
+	addToLevel(2, 2, node3); 
+	addToLevel(2, 1, node4);
+	//addToLevel(3, node5); 
+	//addToLevel(3, node4); 
+	//addToLevel(3, node1); 
+	//addToLevel(3, node6);
+	//addToLevel(2, node4); 
+	//addToLevel(3, node7); 
 
 
 	function createLevel(){
@@ -38,14 +40,17 @@
 		tree.appendChild(newLevel);
 		return newLevel;
 	}
-	/*
+	
 	function createSubsection(level, howMany) {
-		var newSubsection = document.createElement("div"); 
-		newSubsection.setAttribute("class", "level"); 
-		var toAdd = tree.childNodes; 
-		var levelNode = toAdd[1]; 
-		levelNode.appendChild(newSubsection); 
-	}*/
+		for (i = 0; i < howMany; i++){
+			var newSubsection = document.createElement("div"); 
+			newSubsection.setAttribute("class", "level"); 
+			var toAdd = tree.childNodes; 
+			var levelNode = toAdd[level]; 
+			var added = levelNode.appendChild(newSubsection); 	
+			added.style.width = "" + 100/level + "%";
+		}
+	}
 	
 	// Function that creates a new node 
 	function createNode(text, redirect){
@@ -64,7 +69,11 @@
 		return newNode; 
 	}
 
-	function addToLevel(level, node){
-		var toAdd = document.getElementById("Level" + level);
+	function addToLevel(level, subsection, node){
+		var levelNode = document.getElementById("Level" + level); 
+		var toAdd = levelNode.firstChild; 
+		for(i=1; i < subsection; i++){ 
+			toAdd = toAdd.nextSibling;
+		}
 		toAdd.appendChild(node); 
 	}
