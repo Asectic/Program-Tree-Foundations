a942116b17bd5e4174afccc62fedf38851fa2b2f
c4khatta <shamama.khattak@mail.utoronto.ca>
2016-03-01 02:41:50 -0500

    Update tree.js
    Fixed some of previous problems with tree - mainly centered it

--- Webpage Files/tree.js
+++ Webpage Files/tree.js
@@ -6,22 +6,47 @@
 	node1 = createNode("Hello", "http://www.google.com");
 	node2 = createNode("There", "http://www.facebook.com");
 	node3 = createNode("Top", "http://www.hotmail.com");
-	node4 = createNode("hi", "http://www.gmail.com")
+	node4 = createNode("hi", "http://www.gmail.com");
+	node5 = createNode("hi", "http://www.google.com"); 
+	node6 = createNode("hi", "http://www.google.com"); 
+	node7 = createNode("hi", "http://www.google.com"); 
 	level1 = createLevel();
 	level2 = createLevel();
+	level3 = createLevel(); 
+	//createSubsection(2, 1); 
+
 	addToLevel(1, node3);
 	addToLevel(2, node1);
 	addToLevel(2, node2);
+	addToLevel(3, node5); 
+	addToLevel(3, node4); 
+	addToLevel(3, node1); 
+	addToLevel(3, node6);
+	addToLevel(2, node4); 
+	addToLevel(3, node7); 
 
 
 	function createLevel(){
+		// Create Level
 		var newLevel = document.createElement("div");
+		
+		// Set Attributes of that level
 		newLevel.id = "Level" + tree.childNodes.length; 
 		newLevel.setAttribute("class", "level"); 
+		
+		// Append it to the tree and return
 		tree.appendChild(newLevel);
 		return newLevel;
 	}
-
+	/*
+	function createSubsection(level, howMany) {
+		var newSubsection = document.createElement("div"); 
+		newSubsection.setAttribute("class", "level"); 
+		var toAdd = tree.childNodes; 
+		var levelNode = toAdd[1]; 
+		levelNode.appendChild(newSubsection); 
+	}*/
+	
 	// Function that creates a new node 
 	function createNode(text, redirect){
 		// Create new elements needed for node
@@ -41,7 +66,5 @@
 
 	function addToLevel(level, node){
 		var toAdd = document.getElementById("Level" + level);
-		node.id = "nodeFloatsLeft";
-		var levelNodes = toAdd.childNodes.length; 
 		toAdd.appendChild(node); 
 	}
