8ceb3ebe0afd08d0fbe29f557e5eccb854a67c82
c4khatta <shamama.khattak@mail.utoronto.ca>
2016-03-15 17:15:55 -0400

    Added files via upload
    Adding in lesson.js

---
+++ lesson.js
@@ -0,0 +1,6 @@
+	var nextButton = document.getElementById("frontNext");
+	nextButton.setAttribute("class", "button");
+
+	$(nextButton).click(function () {
+    	window.location = "www.google.com";
+	});
