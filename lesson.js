91f28e3dae788ec63a52f2fd061901c5747e4f4c
c4khatta <shamama.khattak@mail.utoronto.ca>
2016-03-15 17:17:00 -0400

    Update lesson.js
    Adding button functions to lesson.js

--- lesson.js
+++ lesson.js
@@ -1,6 +1,17 @@
 	var nextButton = document.getElementById("frontNext");
-	nextButton.setAttribute("class", "button");
+	var backButton = document.getElementById("goBack"); 
+
+	window.onload = function() {
+		 document.getElementById("lessonPortion").style.display = 'block';
+		document.getElementById("exercises").style.display = 'none'; 
+	};
 
 	$(nextButton).click(function () {
-    	window.location = "www.google.com";
+    	    document.getElementById("lessonPortion").style.display = 'none';
+			document.getElementById("exercises").style.display = 'block'; 
 	});
+
+	$(backButton).click(function (){
+			document.getElementById("lessonPortion").style.display = 'block'; 
+			document.getElementById("exercises").style.display = 'none'
+	}); 
