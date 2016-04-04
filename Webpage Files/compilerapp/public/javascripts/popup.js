3dd7a1a084d134a3fe001899db7b00ebb4fb3b36
Asectic <flora.hjlee@gmail.com>
2016-04-04 16:56:54 -0400

    Changed layout of webpage

---
+++ Webpage Files/compilerapp/public/javascripts/popup.js
@@ -0,0 +1,8 @@
+    // Adding in popup function
+    function toggle_visibility(id) {
+           var e = document.getElementById(id);
+           if(e.style.display == 'block')
+              e.style.display = 'none';
+           else
+              e.style.display = 'block';
+    }
